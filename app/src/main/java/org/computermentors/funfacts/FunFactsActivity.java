31e7a32823b0a921bd3a9f046a18f65ecd036ff9
CryoFlar3 <ndukes@computermentors.org>
2018-07-04 14:28:18 -0400

    App changes to a new fact when button is clicked.

--- app/src/main/java/org/computermentors/funfacts/FunFactsActivity.java
+++ app/src/main/java/org/computermentors/funfacts/FunFactsActivity.java
@@ -2,12 +2,32 @@ package org.computermentors.funfacts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class FunFactsActivity extends AppCompatActivity {
+    // Declare our View Variables
+    private TextView factTextView;
+    private Button showFactButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
+
+        // Assign the Views from the layout file to the corresponding variables
+        factTextView = findViewById(R.id.factTextView);
+        showFactButton = findViewById(R.id.showFactButton);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // The button was clicked, so update the fact TextView with a new facr
+                String fact = "Ostriches can run faster than horses";
+                factTextView.setText(fact);
+            }
+        };
+        showFactButton.setOnClickListener(listener);
     }
 }
