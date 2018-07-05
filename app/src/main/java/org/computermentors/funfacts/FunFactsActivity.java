deca598447b3dafaadfeb7b17a010a3434ae9f9b
CryoFlar3 <ndukes@computermentors.org>
2018-07-05 00:38:32 -0400

    Added a log tool

--- app/src/main/java/org/computermentors/funfacts/FunFactsActivity.java
+++ app/src/main/java/org/computermentors/funfacts/FunFactsActivity.java
@@ -2,12 +2,14 @@ package org.computermentors.funfacts;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class FunFactsActivity extends AppCompatActivity {
+    public static final String TAG = FunFactsActivity.class.getSimpleName();
     private FactBook factBook = new FactBook();
     private ColorWheel colorWheel = new ColorWheel();
     // Declare our View Variables
@@ -38,5 +40,7 @@ public class FunFactsActivity extends AppCompatActivity {
             }
         };
         showFactButton.setOnClickListener(listener);
+//        Toast.makeText(this,"Yay! Our Activity was created!", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "We're logging from the onCreate() method");
     }
 }
